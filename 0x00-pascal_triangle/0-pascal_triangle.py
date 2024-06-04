643bff16d70610eac43b80b88e27072e8a517549
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-04 15:42:02 +0100

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -35,7 +35,10 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     for i in range(1, n):
-        row = [1] + [temp_row[j - 1] + temp_row[j] for j in range(1, i) if len(temp_row) > 1] + [1]
+        row = [1] + [
+                temp_row[j - 1] + temp_row[j]
+                for j in range(1, i) if len(temp_row) > 1
+                ] + [1]
         temp_row = row
         triangle.append(row)
 
