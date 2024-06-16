84d929b2e140c9ed303ad30bf772f1cbe17254c1
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-16 22:51:52 +0100

    :tada: file: 0-minoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Minimum Operations
+"""
+
+def minOperations(n):
+    """
+    Calculate the fewest number of operations needed to result in exactly n H characters in the file.
+    """
+    if n <= 1:
+        return 0
+    operations = 0
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            operations += i
+            n = n // i
+        else:
+            i += 1
+    return operations
