a81f1078fb262963fc2dc59749adbcc90c566214
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-20 20:45:23 +0100

    test

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,13 +3,9 @@
 import sys
 import re
 
-
 log_format = re.compile(
-    r'^(\d{1,3}\.){3}\d{1,3} - \['
-    r'(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} [-+]\d{4})\] '
-    r'"GET /projects/260 HTTP/1.1" '
-    r'(\d{3}) '
-    r'(\d+)$'
+    r'^(\d{1,3}\.){3}\d{1,3} - \[(.*?)\] '
+    r'"GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$'
 )
 
 total_file_size = 0
@@ -26,38 +22,49 @@ status_codes = {
 }
 
 
-def update_metrics(line):
-    """
-    Update metrics with the information from the log line
-    """
-    global total_file_size, status_codes
-
+def extract_input(line):
+    """Extracts and parses parts of the log line."""
     match = log_format.match(line)
     if match:
-        file_size = int(match.group(4))
-        total_file_size += file_size
+        return {
+            'status_code': int(match.group(3)),
+            'file_size': int(match.group(4))
+        }
+    return None
 
-        status_code = int(match.group(3))
+
+def update_metrics(info):
+    """Updates metrics with the information from the log line."""
+    global total_file_size, status_codes
+    if info:
+        total_file_size += info['file_size']
+        status_code = info['status_code']
         if status_code in status_codes:
             status_codes[status_code] += 1
 
 
 def print_summary():
-    """
-    Print the metrics summary
-    """
+    """Prints the metrics summary."""
     print(f'File size: {total_file_size}', flush=True)
     for key in sorted(status_codes.keys()):
         if status_codes[key] > 0:
             print(f'{key}: {status_codes[key]}', flush=True)
 
 
-if __name__ == '__main__':
+def main():
+    """Main function to read input and update metrics."""
+    global line_count
     try:
         for line in sys.stdin:
             line_count += 1
-            update_metrics(line.strip())
+            info = extract_input(line.strip())
+            update_metrics(info)
             if line_count % 10 == 0:
                 print_summary()
     except (KeyboardInterrupt, EOFError):
         print_summary()
+
+
+if __name__ == '__main__':
+    main()
+test
