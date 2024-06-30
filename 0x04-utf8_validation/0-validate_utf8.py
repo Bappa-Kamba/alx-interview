6600bb9117582f39385799ef9c158d6e4a18bf93
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-30 22:09:47 +0100

    :zap: "Refactored UTF-8 validation logic in 0-validate_utf8.py"

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,29 +14,26 @@ def validUTF8(data: List) -> bool:
         Returns:
             `True` if the data is a valid string, `False otherwise.
     """
-    if not data:
-        return False
-
-    num_count = 0
+    num_count = 0  # number of continuation bytes expected
     for num in data:
         bin_repr = format(num, '08b')
+        print(bin_repr)
 
         if num_count == 0:
             if bin_repr[0] == '0':
                 continue
             elif bin_repr[:3] == '110':
-                num_count = 2  # a 2-byte character
-
+                num_count = 1  # expecting 1 continuation byte
             elif bin_repr[:4] == '1110':
-                num_count = 3  # a 3-byte character
-
+                num_count = 2  # expecting 2 continuation bytes
             elif bin_repr[:5] == '11110':
-                num_count = 4  # a 4-byte character
+                num_count = 3  # expecting 3 continuation bytes
+            else:
+                return False
 
         else:
             if bin_repr[:2] != '10':
-
                 return False
-            num_count = 0
+            num_count -= 1
 
     return num_count == 0
