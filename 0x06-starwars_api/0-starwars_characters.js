65777f08f92a6771eb60a65172310a196d3097e5
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-13 21:55:54 +0100

    :shirt: fixed liner errors and warnings

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,53 +3,52 @@
 'use_strict';
 const request = require('request');
 
-let movieID = process.argv[2];
+const movieID = process.argv[2];
 
-function fetchMovieDetails(id, callback) {
-    const SWAPI = `https://swapi-api.alx-tools.com/api/films/${id}`;
+function fetchMovieDetails (id, callback) {
+  const SWAPI = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-    request(SWAPI, { json: true }, (err, res, body) => {
-        if (err) {
-            return callback(err);
-        }
-        callback(null, body);
-    });
+  request(SWAPI, { json: true }, (err, res, body) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, body);
+  });
 }
 
-function fetchCharacterDetails(url, callback) {
-    request(url, { json: true }, (err, res, body) => {
-        if (err) return callback(err);
-        callback(null, body);
-    });
+function fetchCharacterDetails (url, callback) {
+  request(url, { json: true }, (err, res, body) => {
+    if (err) return callback(err);
+    callback(null, body);
+  });
 }
 
 fetchMovieDetails(movieID, (err, data) => {
-    if (err) {
-        console.error('Error fetching movie details:', err);
-        return;
+  if (err) {
+    console.error('Error fetching movie details:', err);
+    return;
+  }
+
+  const characterURLs = data.characters;
+  const characterNames = [];
+  let index = 0;
+
+  function fetchNextCharacter () {
+    if (index < characterURLs.length) {
+      fetchCharacterDetails(characterURLs[index], (error, character) => {
+        if (error) {
+          console.log(`Error fetching character details: ${error}`);
+          return;
+        }
+
+        characterNames.push(character.name);
+        console.log(character.name);
+
+        index++;
+        fetchNextCharacter(); // Fetch next character recursively
+      });
     }
+  }
 
-    const characterURLs = data.characters;
-    const characterNames = [];
-    let index = 0;
-
-    function fetchNextCharacter() {
-        if (index < characterURLs.length) {
-            fetchCharacterDetails(characterURLs[index], (error, character) => {
-                if (error) {
-                    console.log(`Error fetching character details: ${error}`);
-                    return;
-                }
-
-                characterNames.push(character.name);
-                console.log(character.name);
-
-                index++;
-                fetchNextCharacter(); // Fetch next character recursively
-            });
-        } else return
-    }
-
-    fetchNextCharacter();
+  fetchNextCharacter();
 });
-
