e76eddcf9d622f3f0a7a69d5d0a3f46cf0a58bc8
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-08-01 15:51:09 +0100

    :zap:  Implementation of making change function

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Making Change Module """
+
+
+def makeChange(coins, total):
+    """
+        Determine the fewest number of coins needed to meet a given total
+
+        Args:
+            coins - list of coins possessed
+            total - total coins to make up
+
+        Returns:
+            0 if total is <= 0, -1 if total cannot be met or fewest number
+            of coins needed to amke total
+    """
+    # Create a DP array with a large initial value
+    dp = [total + 1] * (total + 1)
+    # Base case: to make 0 total, 0 coins are required
+    dp[0] = 0
+
+    # Iterate over each coin
+    for coin in coins:
+        # Update dp array for each total from coin to the target total
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    # If dp[total] is still the large initial value, return -1
+    # Otherwise, return dp[total] as the minimum number of coins needed
+    return dp[total] if dp[total] != total + 1 else -1
